e5b22d4f7d00a817b920dcf854299477c4728e9a
kazuki827 <xm.kazuki827@gmail.com>
2021-12-26 21:11:51 +0900

    Project setup & Auth - Login/Register

---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'save',
+  title: 'Save',
+  type: 'document',
+  fields: [
+    {
+      name: 'postedBy',
+      type: 'PostedBy',
+      type: 'postedBy'
+    },
+    {
+      name: 'userId',
+      type: 'UserId',
+      type: 'string'
+    }
+  ]
+};
